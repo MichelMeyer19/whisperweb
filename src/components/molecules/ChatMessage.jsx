f156e0b30fe6c08e717c8f72709f57d7f9d5b989
JohnWad94 <47334557+JohnWad94@users.noreply.github.com>
2023-11-02 16:14:24 +0100

    to make it run

--- src/components/molecules/ChatMessage.jsx
+++ src/components/molecules/ChatMessage.jsx
@@ -1,42 +1,42 @@
-// import React from "react";
-// import TopicText from "../atoms/TopicText";
-// import UsrName from "../atoms/UsrName";
-// import MsgText from "../atoms/MsgText";
-
-// const ChatMessage = ({ topic, userName, message }) => {
-//   return (
-//     <div className="flex flex-col items-start justify-center bg-white m-5 p-5 rounded w-11/12 max-w-4xl h-32 max-h-32 shadow-lg">
-//       <TopicText text={topic} />
-//       <UsrName text={userName} />
-//       <div className="line-clamp-1">
-//         <MsgText text={message} />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ChatMessage;
-
-// ChatMessage.jsx
-
 import React from "react";
-import { Link } from "react-router-dom";
 import TopicText from "../atoms/TopicText";
 import UsrName from "../atoms/UsrName";
 import MsgText from "../atoms/MsgText";
 
-const ChatMessage = ({ chatId, topic, userName, message }) => {
+const ChatMessage = ({ topic, userName, message }) => {
   return (
-    <Link to={`/chat/${chatId}`}>
-      <div className="flex flex-col items-start justify-center bg-white m-5 p-5 rounded w-11/12 max-w-4xl h-32 max-h-32 shadow-lg">
-        <TopicText text={topic} />
-        <UsrName text={userName} />
-        <div className="line-clamp-1">
-          <MsgText text={message} />
-        </div>
+    <div className="flex flex-col items-start justify-center bg-white m-5 p-5 rounded w-11/12 max-w-4xl h-32 max-h-32 shadow-lg">
+      <TopicText text={topic} />
+      <UsrName text={userName} />
+      <div className="line-clamp-1">
+        <MsgText text={message} />
       </div>
-    </Link>
+    </div>
   );
 };
 
 export default ChatMessage;
+
+ChatMessage.jsx;
+
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import TopicText from "../atoms/TopicText";
+// import UsrName from "../atoms/UsrName";
+// import MsgText from "../atoms/MsgText";
+
+// const ChatMessage = ({ chatId, topic, userName, message }) => {
+//   return (
+//     <Link to={`/chat/${chatId}`}>
+//       <div className="flex flex-col items-start justify-center bg-white m-5 p-5 rounded w-11/12 max-w-4xl h-32 max-h-32 shadow-lg">
+//         <TopicText text={topic} />
+//         <UsrName text={userName} />
+//         <div className="line-clamp-1">
+//           <MsgText text={message} />
+//         </div>
+//       </div>
+//     </Link>
+//   );
+// };
+
+// export default ChatMessage;
