f9e7ad9f38c2654381e6830b962794270490d611
JohnWad94 <47334557+JohnWad94@users.noreply.github.com>
2023-11-06 20:16:50 +0100

    fixed chatbox input placement

--- src/components/organisms/ChatBox.jsx
+++ src/components/organisms/ChatBox.jsx
@@ -4,11 +4,15 @@ import InputText from "../atoms/InputText";
 
 const ChatBox = ({ chatMessages }) => {
   return (
-    <div className="w-11/12 bg-dorian rounded-lg m-2 p-4 overflow-y-auto shadow-lg mb-16 h-full">
-      {chatMessages.map((message, index) => (
-        <ChatMessageDetail key={index} {...message} />
-      ))}
-      <InputText />
+    <div className="w-11/12 bg-dorian rounded-lg m-2 p-4 overflow-y-auto shadow-lg mb-16 h-full flex flex-col">
+      <div className="flex-grow">
+        {chatMessages.map((message, index) => (
+          <ChatMessageDetail key={index} {...message} />
+        ))}
+      </div>
+      <div className="flex items-end">
+        <InputText />
+      </div>
     </div>
   );
 };
