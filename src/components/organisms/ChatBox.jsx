7ad78eda784f7943e88fca9b932db31d6314411b
JohnWad94 <47334557+JohnWad94@users.noreply.github.com>
2023-11-09 20:55:27 +0100

    Fixed chatIO position + most recent chat is focused

--- src/components/organisms/ChatBox.jsx
+++ src/components/organisms/ChatBox.jsx
@@ -1,5 +1,5 @@
 // ChatBox.jsx
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import ChatMessageDetail from "../molecules/ChatMessageDetail";
 import ChatIO from "../molecules/ChatIO";
 
@@ -36,19 +36,30 @@ const MESSAGES = [
 
 const ChatBox = () => {
   const [chatMessages, setChatMessages] = useState(MESSAGES);
+  const chatBoxRef = useRef(null);
 
   const handleSendClick = (newMessage) => {
     setChatMessages((prevChatMessages) => [...prevChatMessages, newMessage]);
   };
 
+  useEffect(() => {
+    // Scroll to the bottom when new messages are added
+    chatBoxRef.current.scrollTop = chatBoxRef.current.scrollHeight;
+  }, [chatMessages]);
+
   return (
-    <div className="w-11/12 bg-dorian rounded-lg m-2 p-4 overflow-y-auto shadow-lg mb-16 h-full flex flex-col">
+    <div
+      className="w-11/12 bg-dorian rounded-lg m-2 p-4 overflow-y-auto shadow-lg mb-16 h-full flex flex-col"
+      ref={chatBoxRef}
+    >
       <div className="flex-grow">
         {chatMessages.map((message, index) => (
           <ChatMessageDetail key={index} {...message} />
         ))}
       </div>
-      <ChatIO onSendClick={handleSendClick} />
+      <div className="mt-auto">
+        <ChatIO onSendClick={handleSendClick} />
+      </div>
     </div>
   );
 };
