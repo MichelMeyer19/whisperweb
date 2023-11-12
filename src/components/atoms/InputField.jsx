605de52e5c727dd4325ac964af52b3661009b6fa
anwb <antonwb@live.dk>
2023-11-12 16:22:58 +0100

    Added autocomplete attributes in Login Flow
    As title says

--- src/components/atoms/InputField.jsx
+++ src/components/atoms/InputField.jsx
@@ -2,6 +2,14 @@
 import React from "react";
 
 const InputField = ({ type, id, value, onChange, placeholder, label }) => {
+  // Determine the correct autocomplete attribute based on input type
+  let autoCompleteValue;
+  if (type === 'email') {
+    autoCompleteValue = 'username';
+  } else if (type === 'password') {
+    autoCompleteValue = 'current-password';
+  }
+
   return (
     <label htmlFor={id}>
       {label}
@@ -13,6 +21,8 @@ const InputField = ({ type, id, value, onChange, placeholder, label }) => {
         placeholder={placeholder}
         className="w-full p-2 border rounded-md mb-4"
         required
+        // Apply the autocomplete attribute dynamically
+        autoComplete={autoCompleteValue}
       />
     </label>
   );
