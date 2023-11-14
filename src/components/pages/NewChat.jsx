b7bfa88074cca5ebf0ecaecabeaf7e63b70fd4bd
MichelMeyer19 <michel.mey1998@gmail.com>
2023-11-14 16:12:10 +0100

    check for current user on NewChat Page

--- src/components/pages/NewChat.jsx
+++ src/components/pages/NewChat.jsx
@@ -12,29 +12,44 @@ export const NewChat = () => {
 
   // define state that contains most relevant topics >> and stores if the topic is clicked or not!
   const [topics, setTopics] = useState([]);
+  // define state to show current user and therefore allow access to DB
+  const [currentUser, setCurrentUser] = useState(null);  
+
 
   // initiate a DB call on each first render of page
   useEffect(() => {
     // call function to retrieve topics to display on page from DB
     fetchTopics();
+    // call function to retrieve current user
+    getCurrentUser();
   }, []);
+
+  // Function that will return current user and also update current username
+  const getCurrentUser = async function () {
+    const currentUser = await Parse.User.current();
+    // Update state variable holding current user
+    setCurrentUser(currentUser);
+    return currentUser;
+  };
 
   // function to retreive topics and create list of dictionaries (incl. topic_id and topic_name)
   async function fetchTopics() {
 
     // query DB to retrieve topics
     let parseQuery = new Parse.Query("Topics");
-    let queryResults = await parseQuery.find();
-    
-    let fetchedTopicList = []
 
-    // Store results as list of dictionaries
-    for (let result of queryResults) {      
-      fetchedTopicList.push({'id':result.id, 'topic_name':result.get(['topic_name']), 'isClicked':false})
-    };
-
-    // set topics to fetched list
-    setTopics(fetchedTopicList)
+    try {
+      let queryResults = await parseQuery.find();
+      let fetchedTopicList = []
+      // Store results as list of dictionaries
+      for (let result of queryResults) {      
+        fetchedTopicList.push({'id':result.id, 'topic_name':result.get(['topic_name']), 'isClicked':false})
+      };
+      // set topics to fetched list
+      setTopics(fetchedTopicList)      
+    } catch (error) {
+      console.log('call failed!!!')
+    }
   }
 
   return (
@@ -50,10 +65,12 @@ export const NewChat = () => {
             <NewChatButton
               text = "Request Themed Chat"
               topics = {topics}
+              currentUser = {currentUser}
             />
             <NewChatButton
               text = "Request General Chat"
               topics = {''}
+              currentUser = {currentUser}
             />
           </div>
         </div>
