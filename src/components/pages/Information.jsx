524f2511cbe91b140a65cd4ddef0af8f773bc42d
anwb <antonwb@live.dk>
2023-11-20 10:30:54 +0100

    Outsourced text from Information page and Our Values as JSON files
    Tile describes

--- src/components/pages/Information.jsx
+++ src/components/pages/Information.jsx
@@ -1,71 +1,15 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Temp from "../template/Temp";
 import PageHeadline from "../atoms/PageHeadline";
 import InformationButton from "../atoms/InformationButton";
 import LogoutButton from "../atoms/LogoutButton";
-
-// Declare the 4 pieces of information incl. title and content. Content has been prompted and written by ChatGPT.
-let initialInformations = [
-  {
-    id: "0",
-    info_header: "Good Ways to Disagree",
-    content: (
-      <p>
-        Remember, it's completely okay to see things differently. The key to a healthy
-        disagreement is to listen actively and respond with respect. Try using phrases
-        like "I see your point, but have you considered..." or "I understand where
-        you're coming from, however...". It's not about winning; it's about understanding
-        each other better. 🤝
-      </p>
-    ),
-    isClicked: false
-  },
-  {
-    id: "1",
-    info_header: "Asking the Right Questions",
-    content: (
-      <p>
-        Curiosity is your superpower! Asking the right questions opens doors to better
-        insights and connections. Keep them open-ended, think "What", "How", and "Why".
-        For example, "What are your thoughts on..." or "How do you feel about...".
-        This invites fuller responses and shows you're genuinely interested. 🔍
-      </p>
-    ),
-    isClicked: false
-  },
-  {
-    id: "2",
-    info_header: "Not Being Stressed by This App",
-    content: (
-      <p>
-        Feeling overwhelmed? Take a moment to breathe. 🌬️ Our app is here to make life
-        easier, not harder. Navigate at your own pace, explore features as you feel
-        comfortable, and remember, we are here to support you, not add to your stress.
-        If you're feeling stuck, reach out to us - we've got your back!
-      </p>
-    ),
-    isClicked: false
-  },
-  {
-    id: "3",
-    info_header: "Creating Safe Spaces",
-    content: (
-      <p>
-        You're in a judgement-free zone. 🛖 Creating safe spaces is about being inclusive,
-        respectful, and kind. It's a shared effort to ensure everyone feels heard and
-        valued. Let's be mindful of the words we use and the support we offer. Together,
-        we can make this space a little haven for all of us.
-      </p>
-    ),
-    isClicked: false
-  }
-];
+import initialInformation from "../utils/articles_information_page.json"
 
 // ChatGPT has been used to ensure that already open information cards will be closed when a new item is opened
 export const Information = () => {
 
   // Assign a state variable where each information card's id is associated to a state (currently selected or not)
-  const [infos, setInfos] = useState(initialInformations);
+  const [infos, setInfos] = useState(initialInformation);
 
   function handleInfoClick(e) {
     
