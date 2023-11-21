60845568c6110cffa48921c504694d45d4b216ef
MichelMeyer19 <michel.mey1998@gmail.com>
2023-11-21 15:48:03 +0100

    remove alerts from logout flow, as they make me angry :D

--- src/components/atoms/LogoutFunction.jsx
+++ src/components/atoms/LogoutFunction.jsx
@@ -12,11 +12,11 @@ const LogoutButton = () => {
       // To verify that current user is now empty, currentAsync can be used
       const currentUser = await Parse.User.current();
       if (currentUser === null) {
-        alert("Logout in process");
+        return true;
+      } else {
+        alert(`Error! ${error.message}`);
+        return false;
       }
-      // Update state variable holding current user (you can add this function as needed)
-      // getCurrentUser();
-      return true;
     } catch (error) {
       alert(`Error! ${error.message}`);
       return false;
@@ -29,9 +29,6 @@ const LogoutButton = () => {
 
     // Check if the user was successfully logged out
     if (loggedOut) {
-      // Inform the user that logout worked
-      window.alert("Log out successful");
-
       // Navigate to the sign-in screen (assuming the route is '/sign-in')
       navigate("/sign-in");
     } else {
