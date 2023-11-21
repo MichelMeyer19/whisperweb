9e2aee27e849c961f2f8d50b1624f6fda83a9af5
MichelMeyer19 <michel.mey1998@gmail.com>
2023-11-21 15:47:37 +0100

    move heading a bit down

--- src/components/atoms/PageHeadline.jsx
+++ src/components/atoms/PageHeadline.jsx
@@ -6,7 +6,7 @@ const PageHeadline = ({ text }) => {
   const truncatedText = truncateText(text, 40); // Set your desired character limit
 
   return (
-    <div className="flex flex-col items-center m-1 max-w-5xl">
+    <div className="flex flex-col items-center m-1 max-w-5xl pt-10">
       <h1 className="font-bold text-3xl text-onyx leading-tight">
         {truncatedText}
       </h1>
