0272b95fc7ca42fd46952311cba2fbecc117a575
JohnWad94 <47334557+JohnWad94@users.noreply.github.com>
2023-11-23 12:06:08 +0100

    fixed topic headline -> now it displays topic_name instead of chatID

--- src/components/pages/Chat.jsx
+++ src/components/pages/Chat.jsx
@@ -1,47 +1,3 @@
-// // components/pages/Chat.jsx
-// import React from "react";
-// import ChatBox from "../organisms/ChatBox";
-// import Temp from "../template/Temp";
-// import PageHeadline from "../atoms/PageHeadline";
-// import Back from "../atoms/BackArrow";
-
-// export const Chat = () => {
-//   const chatMessages = [
-//     {
-//       isStart: true,
-//       userName: "Anonymous Cat",
-//       time: "12:45",
-//       message:
-//         "Lately, I've been feeling a bit overwhelmed with my family's expectations. Do you feel the same? ",
-//       avatarSrc: "/icons/obi.webp",
-//     },
-//     {
-//       isStart: false,
-//       userName: "Anonymous Antilope",
-//       time: "12:46",
-//       message:
-//         "Oh, I'm the youngest in my family. It's like I'm always in the shadow of my older siblings. They've set the bar so high, and everyone expects me to follow in their footsteps.",
-//       avatarSrc: "/icons/anakin.webp",
-//     },
-//     {
-//       isStart: true,
-//       userName: "Anonymous Cat",
-//       time: "12:47",
-//       message:
-//         "That sounds tough. Being the eldest, I feel the pressure to set an example. But I can imagine how being the youngest comes with its own set of challenges. Do you ever talk to your siblings about it?",
-//       avatarSrc: "/icons/obi.webp",
-//     },
-//   ];
-
-//   return (
-//     <Temp>
-//       <Back />
-//       <PageHeadline text="#ChosenOne" />
-//       <ChatBox chatMessages={chatMessages} />
-//     </Temp>
-//   );
-// };
-
 // components/pages/Chat.jsx
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -53,32 +9,45 @@ import Parse from "parse/dist/parse.min.js";
 
 export const Chat = () => {
   const { chatId } = useParams();
-  const [chatMessages, setChatMessages] = useState([]);
+  const [chatData, setChatData] = useState({});
   const currentUser = Parse.User.current();
 
   useEffect(() => {
-    // Fetch chat messages based on the selected chat ID
+    // Fetch chat messages and chat details based on the selected chat ID
     const Messages = Parse.Object.extend("Messages");
     const messagesQuery = new Parse.Query(Messages);
     messagesQuery.equalTo("chat_id", chatId);
-    messagesQuery.ascending("createdAt"); // Fetch messages in ascending order
+    messagesQuery.ascending("createdAt");
 
-    messagesQuery
-      .find()
+    const Chats = Parse.Object.extend("Chats");
+    const chatsQuery = new Parse.Query(Chats);
+    chatsQuery
+      .get(chatId) // Fetch the chat details
+      .then((chat) => {
+        setChatData({
+          topic: chat.get("topic_name"),
+          messages: [],
+        });
+
+        // Fetch messages for the current chat
+        return messagesQuery.find();
+      })
       .then((messages) => {
-        // Process the messages and set them in the state
         const processedMessages = messages.map((message) => ({
           isStart: message.get("sent_by_id") !== currentUser.id,
-          userName: message.get("sent_by_id"), // Assuming sent_by_id represents the user
-          time: message.createdAt.toLocaleTimeString(), // Adjust as needed
+          userName: message.get("sent_by_id"),
+          time: message.createdAt.toLocaleTimeString(),
           message: message.get("text"),
           avatarSrc:
             message.get("sent_by_id") !== currentUser.id
               ? "/icons/obi.webp"
-              : "/icons/anakin.webp", // Set different avatarSrc based on isStart
+              : "/icons/anakin.webp",
         }));
 
-        setChatMessages(processedMessages);
+        setChatData((prevChatData) => ({
+          ...prevChatData,
+          messages: processedMessages,
+        }));
       })
       .catch((error) => {
         console.error("Error fetching chat messages:", error);
@@ -88,8 +57,8 @@ export const Chat = () => {
   return (
     <Temp>
       <Back />
-      <PageHeadline text={`#${chatId}`} />
-      <ChatBox chatMessages={chatMessages} />
+      <PageHeadline text={`#${chatData.topic}`} />
+      <ChatBox chatMessages={chatData.messages} />
     </Temp>
   );
 };
