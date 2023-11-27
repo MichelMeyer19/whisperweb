bc0dad894acdfb6a2a561d0121a769429b1f73ad
anwb <antonwb@live.dk>
2023-11-27 14:59:44 +0100

    Styling for NewChat.jsx done
    Added small layout margins for the input button

--- src/components/molecules/ManualTopicInput.jsx
+++ src/components/molecules/ManualTopicInput.jsx
@@ -1,49 +1,53 @@
 import React from "react";
-import {useState} from 'react';
+import { useState } from "react";
 
 // allows user to manually input a topic
 // used in TopicList.jsx
 
-export const ManualTopicInput = ({topics,setTopics}) => {
+export const ManualTopicInput = ({ topics, setTopics }) => {
+  const [newtopic, setNewTopic] = useState("");
 
-    const [newtopic, setNewTopic] = useState('');
+  function handleTopicInput(e) {
+    setNewTopic(e.target.value);
+  }
 
-    function handleTopicInput(e) {
-        setNewTopic(e.target.value);
+  function handleNewTopicLog(e) {
+    const old_topics = topics.map((topic) => {
+      if (topic.isClicked == true) {
+        return { ...topic, isClicked: false };
+      } else {
+        return topic;
+      }
+    });
+    if (newtopic.length > 3) {
+      const old_and_new_topics = old_topics.concat([
+        { id: topics.length.toString(), topic_name: newtopic, isClicked: true },
+      ]);
+      setTopics(old_and_new_topics);
+      setNewTopic("");
+    } else {
+      alert("New Topic needs to be of sufficient length (< 3 characters)");
+      setTopics(old_topics);
     }
+  }
 
-    function handleNewTopicLog(e) {
-        const old_topics = topics.map(topic => {
-            if (topic.isClicked == true) {
-                return {...topic, isClicked: false};
-            } else {
-                return topic;
-            }
-        });
-        if (newtopic.length > 3) {
-            const old_and_new_topics = old_topics.concat([{id:topics.length.toString(), topic_name:newtopic, isClicked:true}])
-            setTopics(old_and_new_topics)
-            setNewTopic('');
-        } else {
-            alert('New Topic needs to be of sufficient length (< 3 characters)')
-            setTopics(old_topics)
-        }
-    }
-
-    //items-center to center input field
-    return (
-        <div className="flex flex-col items-left">
-            <div className="mx-2 flex ml-7">
-                <input 
-                    name="manualTopicInput" 
-                    className="p-2 pl-1 text-sm text-gray-900 border border-gray-300 rounded-lg w-60" 
-                    value={newtopic}
-                    onChange={handleTopicInput}
-                />
-                <button className="bg-black text-white p-2 pl-1 rounded-lg w-20" onClick={handleNewTopicLog}>
-                        Enter
-                </button>
-            </div>
-        </div>
-    );
+  //items-center to center input field
+  return (
+    <div className="flex flex-col items-left">
+      <div className="mx-2 flex ml-7">
+        <input
+          name="manualTopicInput"
+          className="p-2 pl-1 text-sm text-gray-900 border border-gray-300 rounded-lg w-60 mr-4 focus:outline-none focus:border-black" // Added focus:outline-none and focus:border-black here
+          value={newtopic}
+          onChange={handleTopicInput}
+        />
+        <button
+          className="bg-black text-xs uppercase text-white flex justify-center items-center p-2 rounded-lg w-20"
+          onClick={handleNewTopicLog}
+        >
+          Enter
+        </button>
+      </div>
+    </div>
+  );
 };
