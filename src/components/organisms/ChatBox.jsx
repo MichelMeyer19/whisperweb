47f813a3d13fd2b2cf181cb70ea1e22fd8ee6550
JohnWad94 <47334557+JohnWad94@users.noreply.github.com>
2023-11-29 11:28:44 +0100

    minor changes

--- src/components/organisms/ChatBox.jsx
+++ src/components/organisms/ChatBox.jsx
@@ -1,47 +1,10 @@
-// // ChatBox.jsx
-// import React, { useState, useRef, useEffect } from "react";
-// import ChatMessageDetail from "../molecules/ChatMessageDetail";
-// import ChatIO from "../molecules/ChatIO";
-
-// const ChatBox = ({ initialMessages }) => {
-//   const [chatMessages, setChatMessages] = useState(initialMessages || []);
-//   const chatBoxRef = useRef(null);
-
-//   const handleSendClick = (newMessage) => {
-//     setChatMessages((prevChatMessages) => [...prevChatMessages, newMessage]);
-//   };
-
-//   useEffect(() => {
-//     // Scroll to the bottom when new messages are added
-//     chatBoxRef.current.scrollTop = chatBoxRef.current.scrollHeight;
-//   }, [chatMessages]);
-
-//   return (
-//     <div
-//       className="w-11/12 bg-dorian rounded-lg m-2 p-4 overflow-y-auto shadow-lg mb-16 h-full flex flex-col"
-//       ref={chatBoxRef}
-//     >
-//       <div className="flex-grow">
-//         {chatMessages.map((message, index) => (
-//           <ChatMessageDetail key={index} {...message} />
-//         ))}
-//       </div>
-//       <div className="mt-auto">
-//         <ChatIO onSendClick={handleSendClick} />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ChatBox;
-
 // ChatBox.jsx
 import React, { useState, useRef, useEffect } from "react";
 import ChatMessageDetail from "../molecules/ChatMessageDetail";
 import ChatIO from "../molecules/ChatIO";
 
-const ChatBox = ({ initialMessages }) => {
-  const [chatMessages, setChatMessages] = useState(initialMessages || []);
+const ChatBox = () => {
+  const [chatMessages, setChatMessages] = useState(MESSAGES);
   const chatBoxRef = useRef(null);
 
   const handleSendClick = (newMessage) => {
