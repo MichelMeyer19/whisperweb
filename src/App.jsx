4c8309062d8c95a1f3b05b5800eff3f9f02d65a2
Michel <michel.mey1998@gmail.com>
2023-11-30 13:10:35 +0100

    fixing bugs due to bad merge from me

--- src/App.jsx
+++ src/App.jsx
@@ -26,39 +26,41 @@ function App() {
   const location = useLocation(); // Get the current location
   const [showNavbar, setShowNavbar] = useState(true);
 
-    // Use useEffect to update the showNavbar state based on the route
-    useEffect(() => {
-      // Define an array of paths where the Navbar should not be shown
-      const noNavbarPaths = ['/','/welcome', '/welcome/our-values', '/sign-in', '/sign-in/sign-up'];
-      // Set showNavbar to false if the current path is in the noNavbarPaths array
-      setShowNavbar(!noNavbarPaths.includes(location.pathname));
+  // Use useEffect to update the showNavbar state based on the route
+  useEffect(() => {
+    // Define an array of paths where the Navbar should not be shown
+    const noNavbarPaths = ['/','/welcome', '/welcome/our-values', '/sign-in', '/sign-in/sign-up'];
     
-      if (location.pathname.startsWith("/chat/")) {
-        noNavbarPaths.push(location.pathname);
-      }
-    }, [location]); // Dependency array with location to re-run effect when location changes
+    if (location.pathname.startsWith("/chat/")) {
+      noNavbarPaths.push(location.pathname);
+    }
+    
+    // Set showNavbar to false if the current path is in the noNavbarPaths array
+    setShowNavbar(!noNavbarPaths.includes(location.pathname));
+  }, [location]); // Dependency array with location to re-run effect when location changes
 
-    // JSX structure for the App component
-    return (
-      <div className="App">
-        {/* Conditional rendering of Navbar based on showNavbar state */}
-        {showNavbar && <Navbar />}
-        <Routes>
-          {/* React Router's Routes component for handling page routing */}
-          <Route path="/" element={<Welcome />} />
-          <Route path="/chats-overview" element={<ChatsView />} />
-          <Route path="/new-chat" element={<NewChat />} />
-          <Route path="/newchatrequested" element={<NewChatRequested />} />
-          <Route path="/information" element={<Information />} />
-          <Route path="/chat" element={<Chat />} />
-          <Route path="/welcome" element={<Welcome />} />
-          <Route path="/welcome/our-values" element={<OurValues />} />
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-in/sign-up" element={<SignUp />} />
-        </Routes>
-      </div>
-    );
-  }
+  // JSX structure for the App component
+  return (
+    <div className="App">
+      {/* Conditional rendering of Navbar based on showNavbar state */}
+      {showNavbar && <Navbar />}
+      <Routes>
+        {/* React Router's Routes component for handling page routing */}
+        <Route path="/" element={<Welcome />} />
+        <Route path="/chats-overview" element={<ChatsView />} />
+        <Route path="/new-chat" element={<NewChat />} />
+        <Route path="/newchatrequested" element={<NewChatRequested />} />
+        <Route path="/information" element={<Information />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/chat/:chatId" element={<Chat />} />{" "}
+        <Route path="/welcome" element={<Welcome />} />
+        <Route path="/welcome/our-values" element={<OurValues />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-in/sign-up" element={<SignUp />} />
+      </Routes>
+    </div>
+  );
+}
 
 // Exporting the App component as the default export wrapped in Router to provide location context
 export default function AppWrapper() {
