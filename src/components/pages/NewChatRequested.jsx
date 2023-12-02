d00ee16a4e891882466b20849b41ac9631ac6fe7
Michel <michel.mey1998@gmail.com>
2023-12-02 21:46:51 +0100

    return different values if chat request is success

--- src/components/pages/NewChatRequested.jsx
+++ src/components/pages/NewChatRequested.jsx
@@ -1,9 +1,38 @@
 import React from "react";
+import { useLocation } from 'react-router-dom';
 
 // this page appears whenever a new chat was requested
 
 export const NewChatRequested = () => {
-//        <div className="h-screen flex items-center justify-center">
+
+  const location = useLocation();
+  const currentUrl = location.pathname;
+  console.log(currentUrl);
+
+  const lastFourChars = currentUrl.slice(-4);
+
+  if (lastFourChars == 'true') {
+    return (
+      <div className="h-screen flex items-center justify-center">
+        <div className="mt-80">
+          <div className="flex justify-center">
+            <div className="w-60">
+              <p className="font-bold text-center">You could be matched!</p>
+              <p className="font-light text-center">Please find your new Chat on your Chat Page here.</p>
+            </div>
+          </div>
+  
+          <div className="flex justify-center">
+            <img 
+              src={"/icons/arrow_down.png"} 
+              alt={"Arrow_Down"} 
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="h-screen flex items-center justify-center">
       <div className="mt-80">
