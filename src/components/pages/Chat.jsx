dd851fff646abbbe6ff2db153c3f9c524e79f6a0
Michel <michel.mey1998@gmail.com>
2023-12-08 21:00:48 +0100

    fix topic name on chatpage (for offtopic chats)

--- src/components/pages/Chat.jsx
+++ src/components/pages/Chat.jsx
@@ -9,21 +9,30 @@ import PageSubHeadline from "../atoms/PageSubHeadline";
 import Parse from "parse/dist/parse.min.js";
 
 export const Chat = () => {
-  const [chat_topic, setChatTopic] = useState({});
 
+  const [chat_topic, setChatTopic] = useState({});
   const { chatId } = useParams();
   const currentUser = Parse.User.current();
 
+  // when rendering the page, fetch the chat topic and the other username so it can be displayed in the header
   useEffect(() => {
     fetchChatTopic();
   }, []);
 
+  // fetch the chat topic from the database
   const fetchChatTopic = async function () {
     try {
       const Chats = Parse.Object.extend("Chats");
       const chat_topic = await new Parse.Query(Chats).get(chatId);
 
-      setChatTopic(chat_topic.get("topic_name"));
+      const topic_name = chat_topic.get("topic_name");
+      // if no topic name is set, set the topic to "Off-Topic"
+      if (topic_name == null) {
+        setChatTopic("Off-Topic");
+      } else {
+        setChatTopic(topic_name);
+      }
+
     } catch (error) {
       console.error("Error fetching chat messages:", error);
     }
