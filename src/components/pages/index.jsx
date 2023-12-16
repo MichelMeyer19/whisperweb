5b7a205207ace32b6413266afe501380d7be535f
anwb <antonwb@live.dk>
2023-12-16 16:55:01 +0100

    Documentation

--- src/components/pages/index.jsx
+++ src/components/pages/index.jsx
@@ -1,3 +1,7 @@
+// This file acts as an aggregator or barrel file for exporting components from various sub-modules.
+// It simplifies the import statements in other parts of the application by providing a single point of access.
+
+
 export * from "./ChatsView";
 export * from "./Information";
 export * from "./NewChat";
